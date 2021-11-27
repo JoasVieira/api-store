4c42f389aad831e68c45c153c86374e1fe7ac2f2
JoasVieira <joasdouradina@gmail.com>
2021-11-27 16:11:28 -0300

    configuring cors to allow access from all origins

--- src/App/Routes.php
+++ src/App/Routes.php
@@ -21,6 +21,18 @@ class Routes
     {
         self::$app = AppFactory::create();
 
+        self::$app->options('/{routes:.+}', function ($request, $response, $args) {
+            return $response;
+        });
+
+        self::$app->add(function ($request, $handler) {
+            $response = $handler->handle($request);
+            return $response
+                    ->withHeader('Access-Control-Allow-Origin', '*')
+                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        });
+
         self::$app->group('/category', function (RouteCollectorProxy $group): void {
             $group->get('', CategoryController::class . ':index');
             $group->post('', CategoryController::class . ':create');
